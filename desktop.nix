3dd04e8b8df1ff0fecba7f70282fbd2a17b25db3
Edward Li <jiaxi.edwardli@gmail.com>
2025-05-30 04:57:35 +0000

    Use firefox nightly

--- desktop.nix
+++ desktop.nix
@@ -4,11 +4,20 @@ let
   unfree = import <nixpkgs> {
     config.allowUnfree = true;
   };
+
+  # Mozilla overlay for Firefox Nightly
+  moz-rev = "master";
+  moz-url = builtins.fetchTarball { url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";};
+  nightlyOverlay = (import "${moz-url}/firefox-overlay.nix");
+
+  mozillaNightly = import <nixpkgs> {
+    overlays = [ nightlyOverlay ];
+  };
 in {
   # Desktop packages
   home.packages = with pkgs; [
     # Core Apps
-    firefox
+    mozillaNightly.latest.firefox-nightly-bin
     vlc
 
     # KDE Apps
