49f81068da315cdaaed30423a5c795077ed64db1
Edward Li <jiaxi.edwardli@gmail.com>
2025-05-30 04:29:49 +0000

    Install core desktop apps

--- desktop.nix
+++ desktop.nix
@@ -1,8 +1,20 @@
 { pkgs, ... }:
 
-{
+let
+  unfree = import <nixpkgs> {
+    config.allowUnfree = true;
+  };
+in {
   # Desktop packages
   home.packages = with pkgs; [
+    # Core Apps
+    firefox
+    vlc
 
+    # KDE Apps
+    kdePackages.konsole
+
+    # Unfree Apps
+    unfree.vscode
   ];
 }
